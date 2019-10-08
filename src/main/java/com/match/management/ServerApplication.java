b0382f37669996d01e6002ea85f0f3a70d791d92
Peter Kullmann <p.kullmann@arenae.ch>
2019-10-08 10:53:02 +0200

    OpenAPI swagger url fix for openshift https urls

--- src/main/java/com/match/management/ServerApplication.java
+++ src/main/java/com/match/management/ServerApplication.java
@@ -1,7 +1,10 @@
 package com.match.management;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springdoc.api.OpenApiCustomiser;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
@@ -10,6 +13,23 @@ public class ServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ServerApplication.class, args);
+    }
+
+    @Bean
+    public OpenApiCustomiser customize() {
+        return new OpenApiCustomiser() {
+            @Override
+            public void customise(OpenAPI openAPI) {
+                String url = openAPI.getServers().get(0).getUrl();
+                if (isOpenShiftTTTUrl(url)) {
+                    openAPI.getServers().get(0).setUrl("https:" + url.substring(5));
+                }
+            }
+        };
+    }
+
+    private boolean isOpenShiftTTTUrl(String url) {
+        return url.contains("ttt-match-management");
     }
 
 }
