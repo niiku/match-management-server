8b3e61048e201f0f3b5ab878fcc73ad1831db16a
luechtdiode <roland.seidel@interpolar.ch>
2019-10-10 14:43:29 +0200

    Refactoring: Use builder to initialize Table domainobject

--- src/main/java/com/match/management/domain/table/Table.java
+++ src/main/java/com/match/management/domain/table/Table.java
@@ -2,34 +2,31 @@ package com.match.management.domain.table;
 
 import com.match.management.domain.ddd.Aggregate;
 import com.match.management.domain.match.MatchId;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Aggregate
-@AllArgsConstructor
 public class Table {
     private TableId id;
     private TableManagerId tableManagerId;
-    private List<MatchId> matches;
+    private final List<MatchId> matches = new ArrayList<>();
 
     public MatchId getActiveMatch() {
-        return (matches == null || matches.isEmpty()) ? null : matches.get(0);
+        return (matches.isEmpty()) ? null : matches.get(0);
     }
 
     public boolean removeMatch(MatchId id) {
-        if (matches != null) {
-            return matches.remove(id);
-        }
-        return false;
+        return matches.remove(id);
     }
 
     public void addMatch(MatchId id) {
-        if(matches == null) matches = new ArrayList<>();
         matches.add(id);
     }
 }
